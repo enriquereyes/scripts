b8ac619bb32234974efd92582475667de3451622
enriquereyes <lenriquereyes91@gmail.com>
2018-01-18 02:01:36 -0600

    Fixed indentation in marc.pl

--- samples/marc.pl
+++ samples/marc.pl
@@ -1,20 +1,24 @@
- use MARC::Record;
- my $record = MARC::Record->new();
+#!/usr/bin/env perl
+use MARC::Record;
+my $record = MARC::Record->new();
+
+## add the leader to the record. optional.
+$record->leader(’00903pam 2200265 a 4500’);
  
- ## add the leader to the record. optional.
- $record->leader(’00903pam 2200265 a 4500’);
- 
- ## create an author field.
- my $author = MARC::Field->new(
- ’100’,1,’’,
- a => ’Logan, Robert K.’,
- d => ’1939-’
- );
- $record->append_fields($author);
-  ## create a title field.
- my $title = MARC::Field->new(
- ’245’,’1’,’4’,
- a => ’The alphabet effect /’,
- c => ’Robert K. Logan.’
- );
- $record->append_fields($title); 
+## create an author field.
+my $author = MARC::Field->new(
+  ’100’,1,’’,
+  a => ’Logan, Robert K.’,
+  d => ’1939-’
+);
+
+$record->append_fields($author);
+
+## create a title field.
+my $title = MARC::Field->new(
+  ’245’,’1’,’4’,
+  a => ’The alphabet effect /’,
+  c => ’Robert K. Logan.’
+  );
+
+$record->append_fields($title); 
