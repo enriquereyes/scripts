48aa3fc179f055f4e40937abdbe79630c26f0b2d
enriquereyes <lenriquereyes91@gmail.com>
2018-01-18 01:54:05 -0600

    samples

---
+++ samples/marc.pl
@@ -0,0 +1,20 @@
+ use MARC::Record;
+ my $record = MARC::Record->new();
+ 
+ ## add the leader to the record. optional.
+ $record->leader(’00903pam 2200265 a 4500’);
+ 
+ ## create an author field.
+ my $author = MARC::Field->new(
+ ’100’,1,’’,
+ a => ’Logan, Robert K.’,
+ d => ’1939-’
+ );
+ $record->append_fields($author);
+  ## create a title field.
+ my $title = MARC::Field->new(
+ ’245’,’1’,’4’,
+ a => ’The alphabet effect /’,
+ c => ’Robert K. Logan.’
+ );
+ $record->append_fields($title); 
