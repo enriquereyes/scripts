b286aa5e017102aa0ff322b31160a65a451023ab
enriquereyes <lenriquereyes91@gmail.com>
2018-01-19 00:26:06 -0600

    count_by_extension.ps1 and find_in_drive.ps1

---
+++ file-utils/count_by_extension.ps1
@@ -0,0 +1,8 @@
+﻿#Get all items
+Get-ChildItem -Path $directory -Recurse |
+#Get only files
+Where-Object { !$_.PSIsContainer } |
+#Group by extension
+Group-Object Extension |
+#Get data
+Select-Object @{n="Extension";e={$_.Name -replace '^\.'}}, @{n="Size (MB)";e={[math]::Round((($_.Group | Measure-Object Length -Sum).Sum / 1MB), 2)}}, Count
---
+++ file-utils/find_in_drive.ps1
@@ -0,0 +1,21 @@
+﻿$asg = "", "", ""
+$exp = "", "", ""
+
+for ($i = 0; $i -le $exp.Length; $i++)
+{
+    $f = $asg[$i]
+    $path = "I:\$f"
+    if (test-path -Path $path)
+    {
+        get-childitem $path -recurse | where {$_.name -eq $exp[$i]} | % {
+             Write-Host $_.FullName
+        }   
+    }
+    else
+    {
+        $path = "I:\"
+        get-childitem $path -recurse | where {$_.name -eq $exp[$i]} | % {
+             Write-Host $_.FullName
+        } 
+    }
+}
